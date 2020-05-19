5743a5a1718978272e2e6b9036ba5548dac1147b
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-19 12:54:38 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,15 @@
 class Customer
   attr_accessor :name, :age
   
+  @@all = []
+  
   def initialize(name, age)
     @age = age 
     @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
   end 
 end
