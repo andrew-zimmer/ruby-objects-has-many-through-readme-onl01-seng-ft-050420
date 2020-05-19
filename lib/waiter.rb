57bd57f1db776ca1635a3cb4de926d8dc681b33d
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-19 12:57:25 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,15 @@
 class Waiter
   attr_accessor :name, :years_of_experience
   
+  @@all = []
+  
   def initialize(name, experience)
     @name = name 
     @years_of_experience = experience 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
   end 
 end
